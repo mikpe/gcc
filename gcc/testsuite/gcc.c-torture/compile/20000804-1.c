9b36b128d99bf0b172f3ece59347d06e8b3b4791
Mikael Pettersson <mikpelinux@gmail.com>
2024-07-08 14:02:48 +0200

    cdp1802 testsuite: skip tests that require too many registers
    At -O0 they require more registers than we have.
    
    gcc/testsuite/
    
    	* gcc.c-torture/compile/20000804-1.c: Skip for cdp1802.
    	* gcc.c-torture/compile/pr35318.c: Likewise.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gcc/testsuite/gcc.c-torture/compile/20000804-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20000804-1.c
@@ -3,7 +3,7 @@
 /* { dg-do assemble } */
 /* { dg-skip-if "" { { i?86-*-* x86_64-*-* } && { ia32 && { ! nonpic } } } } */
 /* { dg-skip-if "No 64-bit registers" { m32c-*-* } } */
-/* { dg-skip-if "Not enough 64-bit registers" { pdp11-*-* } { "-O0" } { "" } } */
+/* { dg-skip-if "Not enough 64-bit registers" { pdp11-*-* cdp1802-*-* } { "-O0" } { "" } } */
 /* { dg-xfail-if "Inconsistent constraint on asm" { csky-*-* } { "-O0" } { "" } } */
 /* { dg-xfail-if "Inconsistent constraint on asm" { bpf-*-* } { "-O0" } { "" } } */
 /* { dg-xfail-if "" { h8300-*-* } } */
