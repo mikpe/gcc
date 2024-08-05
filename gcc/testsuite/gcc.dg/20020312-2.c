a3e585ca617b9b6d97589c07e7b9174e7215d2a2
Mikael Pettersson <mikpelinux@gmail.com>
2024-08-05 21:20:58 +0200

    cdp1802 testsuite: fix gcc.dg/20020312-2.c test case
    The gcc.dg/20020312-2.c test case requires each target to have a
    case in a long #if/#elif chain.  Add one for the cdp1802.
    
    gcc/testsuite/
    
    	* gcc.dg/20020312-2.c: Handle the cdp1802 as yet another
    	target that has no PIC register.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gcc/testsuite/gcc.dg/20020312-2.c
+++ gcc/testsuite/gcc.dg/20020312-2.c
@@ -120,6 +120,8 @@ extern void abort (void);
 /* No pic register.  */
 #elif defined(__PRU__)
 /* No pic register.  */
+#elif defined(__CDP1802__)
+/* No pic register.  */
 #else
 # error "Modify the test for your target."
 #endif
