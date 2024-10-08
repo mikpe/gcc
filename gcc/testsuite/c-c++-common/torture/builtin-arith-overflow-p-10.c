995d9114db7521c37087a59c2bff6f29a22e892f
Mikael Pettersson <mikpelinux@gmail.com>
2024-10-08 18:44:57 +0200

    cdp1802 testsuite: skip builtin-arith-overflow tests at -O0
    At -O2 these passs but at -O0 they time out.  Skip them at -O0.
    
    gcc/testsuite/
    
    	* c-c++-common/torture/builtin-arith-overflow-10.c: Skip for cdp1802 at -O0.
    	* c-c++-common/torture/builtin-arith-overflow-8.c: Likewise.
    	* c-c++-common/torture/builtin-arith-overflow-9.c: Likewise.
    	* c-c++-common/torture/builtin-arith-overflow-p-10.c: Likewise.
    	* c-c++-common/torture/builtin-arith-overflow-p-8.c: Likewise.
    	* c-c++-common/torture/builtin-arith-overflow-p-9.c: Likewise.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gcc/testsuite/c-c++-common/torture/builtin-arith-overflow-p-10.c
+++ gcc/testsuite/c-c++-common/torture/builtin-arith-overflow-p-10.c
@@ -1,6 +1,7 @@
 /* Test __builtin_{add,sub}_overflow_p on {,un}signed long int.  */
 /* { dg-do run } */
 /* { dg-skip-if "" { ! run_expensive_tests }  { "*" } { "-O0" "-O2" } } */
+/* { dg-skip-if "too slow"  { cdp1802-*-* } { "-O0" } { "" } } */
 
 typedef signed long int S;
 typedef unsigned long int U;
