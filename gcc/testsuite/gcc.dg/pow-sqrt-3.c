8e39c13af2b201f5da8fd0afce2faefb2c44f10e
Mikael Pettersson <mikpelinux@gmail.com>
2024-10-13 16:31:20 +0200

    cdp1802 testsuite: skip pow-sqrt-[123].c
    Their code size doesn't fit in a 16-bit address space.
    
    gcc/testsuite/
    
    	* gcc.dg/pow-sqrt-1.c: Skip for cdp1802.
    	* gcc.dg/pow-sqrt-2.c: Likewise.
    	* gcc.dg/pow-sqrt-3.c: Likewise.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gcc/testsuite/gcc.dg/pow-sqrt-3.c
+++ gcc/testsuite/gcc.dg/pow-sqrt-3.c
@@ -1,6 +1,7 @@
 /* { dg-do run } */
 /* { dg-options "-O2 -ffast-math --param max-pow-sqrt-depth=3" } */
 /* { dg-require-effective-target double64plus } */
+/* { dg-skip-if "too large" { cdp1802-*-* } } */
 
 #define EXPN (1.25)
 #include "pow-sqrt.x"
