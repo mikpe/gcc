5fbbc5782ec46a7de11f9eed88b62d4cb85e31f7
Mikael Pettersson <mikpelinux@gmail.com>
2024-10-14 15:40:16 +0200

    cdp1802 testsuite: make fam-in-union-alone-in-struct-{2,bos}.c work on int16 targets [ONLY GCC-15]
    The tests assume plain int is 32 bits / 4 bytes.  Adjust them to
    use explicit 32-bit types and constants.
    
    gcc/testsuite/
    
    	* c-c++-common/fam-in-union-alone-in-struct-2.c: Use
    	explicit 32-bit types and constants.
    	* c-c++-common/fam-in-union-alone-in-struct-bos.c: Likewise.
    
    Signed-off-by: Mikael Pettersson <mikpelinux@gmail.com>

--- gcc/testsuite/c-c++-common/fam-in-union-alone-in-struct-bos.c
+++ gcc/testsuite/c-c++-common/fam-in-union-alone-in-struct-bos.c
@@ -5,27 +5,27 @@
 
 union with_fam_1 {
   char a;
-  int b[]; 
+  __INT32_TYPE__ b[]; 
 } with_fam_1_v = {.b = {1, 2, 3, 4, 5}};
 
 union with_fam_2 {
-  int a;
+  __INT32_TYPE__ a;
   char b[];  
-} with_fam_2_v = {.a = 0x1f2f3f4f};
+} with_fam_2_v = {.a = __INT32_C(0x1f2f3f4f)};
 
 union with_fam_3 {
   char a[];  
-  int b[];  
-} with_fam_3_v = {.b = {0x1f2f3f4f, 0x5f6f7f7f}};
+  __INT32_TYPE__ b[];  
+} with_fam_3_v = {.b = {__INT32_C(0x1f2f3f4f), __INT32_C(0x5f6f7f7f)}};
 
 struct only_fam {
-  int b[]; 
+  __INT32_TYPE__ b[]; 
 } only_fam_v = {{7, 11}};
 
 struct only_fam_2 {
   unsigned int : 2;
   unsigned int : 3;
-  int b[]; 
+  __INT32_TYPE__ b[]; 
 } only_fam_2_v = {{7, 11}};
 
 int main ()
