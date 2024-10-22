36049766ad7bb65275cfdaeb6b6b7a4985d5272b
Mikael Pettersson <mikpelinux@gmail.com>
2024-10-22 12:49:13 +0200

    cdp1802 target: turn add/sub with a small constant into a sequence of inc/dec
    Reduces libgcc .text size by 1.5%

--- gcc/config/cdp1802/cdp1802-protos.h
+++ gcc/config/cdp1802/cdp1802-protos.h
@@ -43,6 +43,7 @@ extern void cdp1802_print_operand_address (FILE *, rtx);
 extern poly_int64 cdp1802_push_rounding (poly_int64);
 extern bool cdp1802_regno_ok_for_base_p (int);
 extern enum reg_class cdp1802_regno_reg_class (int);
+extern const char *cdp1802_template_incdec (bool, rtx);
 extern const char *cdp1802_template_movdf_r_r (rtx, rtx);
 extern const char *cdp1802_template_movsf_r_r (rtx, rtx);
 
