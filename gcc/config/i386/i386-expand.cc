1e544e0075ad84301f69e792af2926647b930342
Jakub Jelinek <jakub@redhat.com>
2025-12-20 11:58:25 +0100

    i386: Fix up expansion of 2 keylocker and one user_msr builtin [PR123217]
    target can be especially at -O0 a MEM, not just a REG, and most of the
    ix86_expand_builtin spots which use target and can't support MEM
    destinations deal with it properly, except these 3 spots don't.
    
    Fixed thusly, when we change target to a new pseudo, the caller will
    take care of storing that pseudo into the MEM, and this is the
    solution other spots with similar requirements use in the function.
    
    2025-12-20  Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/123217
    	* config/i386/i386-expand.cc (ix86_expand_builtin)
    	<case IX86_BUILTIN_ENCODEKEY128U32,
    	case IX86_BUILTIN_ENCODEKEY256U32>: Set target to a new pseudo even
    	if it is non-NULL but doesn't satisfy register_operand predicate.
    
    	* gcc.target/i386/keylocker-pr123217.c: New test.
    
    (cherry picked from commit 34285402005b19f7ac2d069e4f5e161e86318b3b)

--- gcc/config/i386/i386-expand.cc
+++ gcc/config/i386/i386-expand.cc
@@ -13170,7 +13170,7 @@ ix86_expand_builtin (tree exp, rtx target, rtx subtarget,
 	for (i = 0; i < 3; i++)
 	  xmm_regs[i] = gen_rtx_REG (V2DImode, GET_SSE_REGNO (i));
 
-	if (target == 0)
+	if (target == 0 || !register_operand (target, SImode))
 	  target = gen_reg_rtx (SImode);
 
 	emit_insn (gen_encodekey128u32 (target, op0));
@@ -13213,7 +13213,7 @@ ix86_expand_builtin (tree exp, rtx target, rtx subtarget,
 	for (i = 0; i < 4; i++)
 	  xmm_regs[i] = gen_rtx_REG (V2DImode, GET_SSE_REGNO (i));
 
-	if (target == 0)
+	if (target == 0 || !register_operand (target, SImode))
 	  target = gen_reg_rtx (SImode);
 
 	emit_insn (gen_encodekey256u32 (target, op0));
