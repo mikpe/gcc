fb6fa39311ab8e9d8cc43e2fb53e9de50bf41b1b
Eric Botcazou <ebotcazou@adacore.com>
2026-03-04 20:43:02 +0100

    ada: Reject non-primitive operations in Finalizable aspect
    The implementation does not support them and allowing them would not bring
    any significant benefit.
    
    gcc/ada/ChangeLog:
    
    	* doc/gnat_rm/gnat_language_extensions.rst
    	(Generalized Finalization): Document the new restriction.
    	* sem_ch13.adb (Resolve_Finalizable_Argument): Adjust wording of
    	error message.
    	(Resolve_Finalization_Procedure.Is_Finalizable_Primitive): Require
    	the procedure to be a primitive operation.
    	* gnat_rm.texi: Regenerate.

--- gcc/ada/sem_ch13.adb
+++ gcc/ada/sem_ch13.adb
@@ -16885,6 +16885,7 @@ package body Sem_Ch13 is
       begin
          return Ekind (E) = E_Procedure
            and then Scope (E) = Scope (Typ)
+           and then Is_Primitive (E)
            and then Present (First_Formal (E))
            and then Ekind (First_Formal (E)) = E_In_Out_Parameter
            and then Etype (First_Formal (E)) = Typ
@@ -16938,7 +16939,7 @@ package body Sem_Ch13 is
       end if;
 
       Error_Msg_N
-        ("finalizable primitive must be local procedure whose only formal " &
+        ("subprogram must denote primitive procedure whose only formal " &
          "parameter has mode `IN OUT` and is of the finalizable type", N);
    end Resolve_Finalizable_Argument;
 
